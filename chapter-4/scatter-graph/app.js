dc166f4335b0760d5749ef8542c345557f6a6d61
jooo0922 <2014102458@khu.ac.kr>
2022-04-16 01:03:54 +0900

    add: Init app

---
+++ chapter-4/scatter-graph/app.js
@@ -0,0 +1,8 @@
+const scatterData = [
+  { friends: 5, salary: 22000 },
+  { friends: 3, salary: 18000 },
+  { friends: 10, salary: 88000 },
+  { friends: 0, salary: 180000 },
+  { friends: 27, salary: 56000 },
+  { friends: 8, salary: 74000 },
+];
