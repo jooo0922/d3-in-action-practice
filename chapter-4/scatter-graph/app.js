68a87010acb6ebcb3dca7a16232b00921d8adb88
jooo0922 <2014102458@khu.ac.kr>
2022-04-19 22:06:36 +0900

    add: Use .ticks() and .tickSize() for grid

--- chapter-4/scatter-graph/app.js
+++ chapter-4/scatter-graph/app.js
@@ -16,15 +16,16 @@ const yExtent = d3.extent(scatterData, (d) => d.friends);
 const xScale = d3.scaleLinear().domain(xExtent).range([0, 500]);
 const yScale = d3.scaleLinear().domain(yExtent).range([0, 500]);
 
-const xAxis = d3.axisBottom().scale(xScale); // 축 레이블 요소 생성하는 함수를 리턴해 줌.
+const xAxis = d3.axisBottom().scale(xScale).tickSize(500).ticks(4); // 축 레이블 요소 생성하는 함수를 리턴해 줌.
 d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis); // <g> 요소를 하나 생성하고, 거기에서 .call(xAxis) 로 축 레이블 생성 함수 호출!
 // xAxis(d3.select("svg").append("g").attr("id", "xAxisG")); // 이렇게 호출해줘도 동일함
 
-const yAxis = d3.axisRight().scale(yScale);
+const yAxis = d3.axisRight().scale(yScale).tickSize(500).ticks(16); // tickSize(500) 으로 눈금의 길이를 500, 즉 svg 화폭 사이즈만큼으로 지정함. -> 격자 형태가 그려짐.
 d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
 
-d3.selectAll("path.domain").style("fill", "none").style("stroke", "black"); // 기다란 축의 스타일을 지정함
-d3.selectAll("line").style("stroke", "black"); // 축에 달린 눈금들의 스타일을 지정함.
+// 이런 스타일 값들은 가급적 css 파일에서 적용해주는 게 좋다고 함.
+// d3.selectAll("path.domain").style("fill", "none").style("stroke", "black"); // 기다란 축의 스타일을 지정함
+// d3.selectAll("line").style("stroke", "black"); // 축에 달린 눈금들의 스타일을 지정함.
 
 // d3.select("#xAxisG").attr("transform", "translate(0, 500)"); 이런 식으로 <g> 요소를 움직여서 축을 이동시키는 방법도 있음.
 
